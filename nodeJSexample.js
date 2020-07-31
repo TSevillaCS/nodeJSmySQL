ef1a965fb703af7b2eec330ff5ab87316690379f
TSevillaCS <tsevilla12@gmail.com>
2020-07-31 14:27:55 -0700

    Update nodeJSexample.js

--- nodeJSexample.js
+++ nodeJSexample.js
@@ -11,6 +11,6 @@ con.connect(function(err) {
   console.log("Connect to mySQLServer Success");
   con.query("CREATE DATABASE firstDB", function (err, result) {
     if (err) throw err;
-    console.log("Database created");
+    console.log("Database was created");
   });
 });
